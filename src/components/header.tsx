ccb69b08c644ee747ffd811f233c1422b754426b
tyautyau56 <i19yanagi@gmail.com>
2020-12-28 10:38:13 +0900

    add header.module.scss and header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+//TODO: Complete the header.
+import React from "react";
+
+import style from "../style/header.module.scss"
+
+const setHeader: React.FC = () => (
+    <header>
+        <div>
+            <h2 className={style.h2}>tyautyau56's portfolio</h2>
+        </div>
+    </header>
+)
+
+export default setHeader
