6cdd712b6caf7dd3dcdf47d81dc1cba0fcef0cfc
tyautyau56 <i19yanagi@gmail.com>
2020-12-28 11:14:31 +0900

    import FC

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 //TODO: Complete the header.
-import React from "react";
+import React, { FC } from "react";
 
 import style from "../style/header.module.scss"
 
-const setHeader: React.FC = () => (
+const setHeader: FC = () => (
     <header>
         <div>
             <h2 className={style.h2}>tyautyau56's portfolio</h2>
