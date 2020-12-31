0f9f724650c0df536bff1e4ae3bd515dc504a7e9
tyautyau56 <i19yanagi@gmail.com>
2020-12-31 16:49:14 +0900

    add store.ts

---
+++ src/components/store.ts
@@ -0,0 +1,24 @@
+export const store = [
+    {
+        id: 1,
+        image: '~/resources/images/01.jpg',
+        description: ''
+    },
+    {
+        id: 2,
+        image: '~/resource/images/02.jpg',
+        description: ''
+    },
+    {
+        id: 3,
+        image: '~/resource/images/03.jpg',
+        description: ''
+    },
+    {
+        id: 4,
+        image: '~/resource/images/04.jpg',
+        description: ''
+    }
+]
+
+export default store
