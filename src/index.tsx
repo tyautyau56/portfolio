e6a2d3d06571120504337835345f47aaed7ff85f
tyautyau56 <i19yanagi@gmail.com>
2021-01-02 21:13:37 +0900

    add section

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,26 @@ import ReactDOM from 'react-dom'
 // components
 import App from './components/App'
 import Header from './components/header'
+import PerformanceGallery from './components/Performance/PerformanceGallery'
 
 // global style
-import './index.scss'
+import './global.scss'
 
 ReactDOM.render(
     <StrictMode>
         <Header />
-        <App />
+        <div id="main">
+            <section id="section_1" className="introduce">
+                <h2>Self-Introduction</h2>
+            </section>
+            <section id="section_2" className="education">
+                <h2>Education Background</h2>
+            </section>
+            <section id="section_3" className="performance">
+                <h2>Recent Work</h2>
+                <PerformanceGallery />
+            </section>
+        </div>
     </StrictMode>,
     document.getElementById('root')
 )
