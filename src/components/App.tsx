56080e33a4beac0f92db2fb8095acbc13e64979a
tyautyau56 <i19yanagi@gmail.com>
2021-01-05 14:25:06 +0900

    index.tsx to App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,59 @@
-import React, { FC } from 'react'
-import styles from '../style/App.module.scss'
+import React, { StrictMode } from 'react'
 
-const App: FC = () => (
-    <div>
-        <h1 className={styles.title}>Hello, world!</h1>
-    </div>
-)
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, fab)
+
+// components
+import Header from '../components/header'
+import PerformanceGallery from '../components/Performance/PerformanceGallery'
+import Loder from '../components/loading'
+import Footer from '../components/footer'
+
+// global style
+import '../global.scss'
+
+class App extends React.Component<any, any> {
+    render() {
+        return (
+            <StrictMode>
+                {/*<Loder />*/}
+                <Header />
+                <div id='main'>
+                    <section id='section_1' className='introduce'>
+                        <h2>Self-Introduction</h2>
+                        <p>
+                            web開発やドラムをしている高専生です.いつもはNITTCの同学年でやっている
+                            <a
+                                href='https://github.com/tokuyama-it'
+                                target='_blank'
+                                rel='noreferrer'>
+                                ITをむさぼる会
+                            </a>
+                            で活動をしています. TypeScriptとRustを勉強中です.
+                        </p>
+                    </section>
+                    <section id='section_2' className='education'>
+                        <h2>Education Background</h2>
+                        <ul>
+                            <li>2019年 3月 周南市立須々万中学校卒業</li>
+                            <li>2019年 4月 徳山工業高等専門学校 情報電子工学科 入学</li>
+                            <li>
+                                2019年〜現在 徳山工業高等専門学校 情報電子工学科 在学中
+                            </li>
+                        </ul>
+                    </section>
+                    <section id='section_3' className='performance'>
+                        <h2>Recent Work</h2>
+                        <PerformanceGallery />
+                    </section>
+                </div>
+                <Footer />
+            </StrictMode>
+        )
+    }
+}
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,9 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
-// fontawesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, fab)
-
-// components
-import Header from './components/header'
-import PerformanceGallery from './components/Performance/PerformanceGallery'
-import Loder from './components/loading'
-import Footer from './components/footer'
-
-// global style
-import './global.scss'
+import App from './components/App'
 
 render(
-    <StrictMode>
-        {/*<Loder />*/}
-        <Header />
-        <div id='main'>
-            <section id='section_1' className='introduce'>
-                <h2>Self-Introduction</h2>
-                <p>
-                    web開発やドラムをしている高専生です.いつもはNITTCの同学年でやっている
-                    <a
-                        href='https://github.com/tokuyama-it'
-                        target='_blank'
-                        rel='noreferrer'>
-                        ITをむさぼる会
-                    </a>
-                    で活動をしています. TypeScriptとRustを勉強中です.
-                </p>
-            </section>
-            <section id='section_2' className='education'>
-                <h2>Education Background</h2>
-                <ul>
-                    <li>2019年 3月 周南市立須々万中学校卒業</li>
-                    <li>2019年 4月 徳山工業高等専門学校 情報電子工学科 入学</li>
-                    <li>
-                        2019年〜現在 徳山工業高等専門学校 情報電子工学科 在学中
-                    </li>
-                </ul>
-            </section>
-            <section id='section_3' className='performance'>
-                <h2>Recent Work</h2>
-                <PerformanceGallery />
-            </section>
-        </div>
-        <Footer />
-    </StrictMode>,
+    <App />,
     document.getElementById('root')
 )
