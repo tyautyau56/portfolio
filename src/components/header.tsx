8c88fc5040fb72a6c615dc608f6b20285ba3f741
tyautyau56 <tyautyau56@gmail.com>
2021-01-24 22:37:55 +0900

    change background color to white from black

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,36 +5,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../style/header.scss'
 
 const setHeader: FC = () => (
-    <header>
-        <div className='wrapper'>
-            <h1 className='title'>tyautyau56</h1>
-            <ul>
-                <li>
-                    <a
-                        href='https://twitter.com/tyautyau56'
-                        target='_blank'
-                        rel='noreferrer'>
-                        <FontAwesomeIcon
-                            icon={['fab', 'twitter-square']}
-                            className='fa-2x twitter'
-                        />
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href='https://github.com/tyautyau56'
-                        target='_blank'
-                        rel='noreferrer'>
-                        <FontAwesomeIcon
-                            icon={['fab', 'github-square']}
-                            className='fa-2x github'
-                        />
-                    </a>
-                </li>
-            </ul>
-            <FontAwesomeIcon icon={['fas', 'arrow-down']} />
-        </div>
-    </header>
+    <section className='section_header'>
+        <header>
+            <div className='wrapper'>
+                <h1 className='title'>tyautyau56</h1>
+                <ul>
+                    <li>
+                        <a
+                            href='https://twitter.com/tyautyau56'
+                            target='_blank'
+                            rel='noreferrer'>
+                            <FontAwesomeIcon
+                                icon={['fab', 'twitter-square']}
+                                className='fa-2x twitter'
+                            />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='https://github.com/tyautyau56'
+                            target='_blank'
+                            rel='noreferrer'>
+                            <FontAwesomeIcon
+                                icon={['fab', 'github-square']}
+                                className='fa-2x github'
+                            />
+                        </a>
+                    </li>
+                </ul>
+                <a href='#section_1'>
+                    <FontAwesomeIcon icon={['fas', 'arrow-down']} />
+                </a>
+            </div>
+        </header>
+    </section>
 )
 
 export default setHeader
