b06ff3069d72983d5d7700f6f1c245f5e8d5d42f
tyautyau56 <tyautyau56@gmail.com>
2021-01-26 00:53:50 +0900

    add icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,38 +6,41 @@ import '../style/header.scss'
 
 const setHeader: FC = () => (
     <section className='section_header'>
-        <header>
-            <div className='wrapper'>
-                <h1 className='title'>tyautyau56</h1>
-                <ul>
-                    <li>
-                        <a
-                            href='https://twitter.com/tyautyau56'
-                            target='_blank'
-                            rel='noreferrer'>
-                            <FontAwesomeIcon
-                                icon={['fab', 'twitter-square']}
-                                className='fa-2x twitter'
-                            />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href='https://github.com/tyautyau56'
-                            target='_blank'
-                            rel='noreferrer'>
-                            <FontAwesomeIcon
-                                icon={['fab', 'github-square']}
-                                className='fa-2x github'
-                            />
-                        </a>
-                    </li>
-                </ul>
-                <a href='#main'>
-                    <FontAwesomeIcon icon={['fas', 'arrow-down']} />
-                </a>
-            </div>
-        </header>
+        <div className='wrapper'>
+            <img
+                src='../../resources/icon.png'
+                alt="tyautyau56's avatar"
+                className='icon'
+            />
+            <h1 className='title'>tyautyau56</h1>
+            <ul>
+                <li>
+                    <a
+                        href='https://twitter.com/tyautyau56'
+                        target='_blank'
+                        rel='noreferrer'>
+                        <FontAwesomeIcon
+                            icon={['fab', 'twitter-square']}
+                            className='fa-2x twitter'
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://github.com/tyautyau56'
+                        target='_blank'
+                        rel='noreferrer'>
+                        <FontAwesomeIcon
+                            icon={['fab', 'github-square']}
+                            className='fa-2x github'
+                        />
+                    </a>
+                </li>
+            </ul>
+            <a href='#main'>
+                <FontAwesomeIcon icon={['fas', 'arrow-down']} />
+            </a>
+        </div>
     </section>
 )
 
