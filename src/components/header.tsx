29013d9869d5f9a19fadac0ee5aa28cf065caa4e
tyautyau56 <tyautyau56@gmail.com>
2021-01-27 08:56:39 +0900

    add images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,11 +8,18 @@ const setHeader: FC = () => (
     <section className='section_header'>
         <div className='wrapper'>
             <img
-                src='../../resources/icon.png'
+                src='/resources/icon.png'
                 alt="tyautyau56's avatar"
                 className='icon'
             />
             <h1 className='title'>tyautyau56</h1>
+            <div className="badge">
+                <img src="https://img.shields.io/badge/age-17-9cf.svg?style=for-the-badge" alt="age"/>
+                <img src="https://img.shields.io/badge/Occupation-student-success.svg?style=for-the-badge" alt="occupation"/><br/>
+                <img src="https://img.shields.io/badge/OS-macOS-critical.svg?style=for-the-badge" alt="os"/>
+                <img src="https://img.shields.io/badge/Apple-respect-orange.svg?style=for-the-badge&logo=Apple" alt="apple"/>
+                <img src="https://img.shields.io/badge/editor-webstorm-ff69b4.svg?style=for-the-badge" alt="editor"/>
+            </div>
             <ul>
                 <li>
                     <a
@@ -20,7 +27,7 @@ const setHeader: FC = () => (
                         target='_blank'
                         rel='noreferrer'>
                         <FontAwesomeIcon
-                            icon={['fab', 'twitter-square']}
+                            icon={['fab', 'twitter']}
                             className='fa-2x twitter'
                         />
                     </a>
@@ -31,14 +38,14 @@ const setHeader: FC = () => (
                         target='_blank'
                         rel='noreferrer'>
                         <FontAwesomeIcon
-                            icon={['fab', 'github-square']}
+                            icon={['fab', 'github']}
                             className='fa-2x github'
                         />
                     </a>
                 </li>
             </ul>
             <a href='#main'>
-                <FontAwesomeIcon icon={['fas', 'arrow-down']} />
+                <FontAwesomeIcon icon={['fas', 'chevron-circle-down']} className="fa-2x arrow-icon" />
             </a>
         </div>
     </section>
