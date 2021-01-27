aef78d262bedf6c86707d26d7f0d8228f1de6565
tyautyau56 <tyautyau56@gmail.com>
2021-01-28 00:34:05 +0900

    format

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-// TODO: Complete the header.
 import React, { FC } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -9,18 +8,30 @@ import icon from '/resources/icon.png'
 const setHeader: FC = () => (
     <section className='section_header'>
         <div className='wrapper'>
-            <img
-                src={icon}
-                alt="tyautyau56's avatar"
-                className='icon'
-            />
+            <img src={icon} alt="tyautyau56's avatar" className='icon' />
             <h1 className='title'>tyautyau56</h1>
-            <div className="badge">
-                <img src="https://img.shields.io/badge/age-17-9cf.svg?style=for-the-badge" alt="age"/>
-                <img src="https://img.shields.io/badge/Occupation-student-success.svg?style=for-the-badge" alt="occupation"/><br/>
-                <img src="https://img.shields.io/badge/OS-macOS-critical.svg?style=for-the-badge" alt="os"/>
-                <img src="https://img.shields.io/badge/Apple-respect-orange.svg?style=for-the-badge&logo=Apple" alt="apple"/>
-                <img src="https://img.shields.io/badge/editor-webstorm-ff69b4.svg?style=for-the-badge" alt="editor"/>
+            <div className='badge'>
+                <img
+                    src='https://img.shields.io/badge/age-17-9cf.svg?style=for-the-badge'
+                    alt='age'
+                />
+                <img
+                    src='https://img.shields.io/badge/Occupation-student-success.svg?style=for-the-badge'
+                    alt='occupation'
+                />
+                <br />
+                <img
+                    src='https://img.shields.io/badge/OS-macOS-critical.svg?style=for-the-badge'
+                    alt='os'
+                />
+                <img
+                    src='https://img.shields.io/badge/Apple-respect-orange.svg?style=for-the-badge&logo=Apple'
+                    alt='apple'
+                />
+                <img
+                    src='https://img.shields.io/badge/editor-webstorm-ff69b4.svg?style=for-the-badge'
+                    alt='editor'
+                />
             </div>
             <ul>
                 <li>
@@ -47,7 +58,10 @@ const setHeader: FC = () => (
                 </li>
             </ul>
             <a href='#main'>
-                <FontAwesomeIcon icon={['fas', 'chevron-circle-down']} className="fa-2x arrow-icon" />
+                <FontAwesomeIcon
+                    icon={['fas', 'chevron-circle-down']}
+                    className='fa-2x arrow-icon'
+                />
             </a>
         </div>
     </section>
