3afcfe1c5706133585cd780c14ba3e0daed6e43c
tyautyau56 <tyautyau56@gmail.com>
2021-02-01 01:30:43 +0900

    change webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -96,7 +96,7 @@ module.exports = {
     },
     devServer:{
         historyApiFallback: true,
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname, "dist")
     },
     plugins: [
         new HtmlWebpackPlugin({
