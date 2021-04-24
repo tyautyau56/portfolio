94545365246267167537465d34baeea637ae2ed7
tyautyau56 <tyautyau56@gmail.com>
2021-04-25 00:24:44 +0900

    add react-router

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,4 +1,5 @@
 import React, { StrictMode } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -7,14 +8,22 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fas, fab)
 
 // Component
-import Content from './Content'
-import Projects from './Projects'
 import Top from './Top'
+import About from './About'
+import Skill from './Skill'
+import Projects from './Projects'
 
 const App: React.VFC = () => {
     return (
         <StrictMode>
-            <Top />
+            <div className='md:flex'>
+                <Top />
+                <Router>
+                    <Route exact path='/' component={About} />
+                    <Route path='/skill' component={Skill} />
+                    <Route path='/project' component={Projects} />
+                </Router>
+            </div>
         </StrictMode>
     )
 }
