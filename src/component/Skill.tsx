c5b786e8a9384433cd8ec7f87002a1a7ab07dc84
tyautyau56 <tyautyau56@gmail.com>
2021-05-01 22:23:47 +0900

    change Skill.tsx

--- src/component/Skill.tsx
+++ src/component/Skill.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { SKILL_DETAIL as lang_detail } from './store/skilldetail'
 
 const Skill: React.VFC = () => {
-    return <div></div>
+    return (
+        <div className='pt-16 p-8'>
+            <div className='border-teal-100 border-4 rounded-xl shadow-xl p-4'>
+                <h2 className='font-medium text-gray-600 text-3xl p-4'>
+                    Skill
+                </h2>
+                <hr />
+            </div>
+        </div>
+    )
 }
 
 export default Skill
