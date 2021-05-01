7306fe980d19cacd47c7c7a2f07bee25928a8db6
tyautyau56 <tyautyau56@gmail.com>
2021-05-01 22:21:45 +0900

    change Content.tsx

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -1,36 +1,20 @@
 import React from 'react'
 
-const Content = () => {
+type Props = {
+    title: string
+    children?: React.ReactNode
+}
+
+const Content: React.VFC<Props> = (prop) => {
     return (
-        <div>
-            <section>
-                <article>
-                    <h2>Self-Introduction</h2>
-                    <p>
-                        web開発やドラムをしている高専生です.いつもはNITTCの同学年でやっている
-                        <a
-                            href='https://github.com/tokuyama-it'
-                            target='_blank'
-                            rel='noreferrer'>
-                            ITをむさぼる会
-                        </a>
-                        で活動中. TypeScriptとRustを勉強中です.
-                    </p>
-                </article>
-                <article>
-                    <h2>Education Background</h2>
-                    <ul>
-                        <li>2019年 3月 周南市立須々万中学校卒業</li>
-                        <li>
-                            2019年 4月 徳山工業高等専門学校 情報電子工学科 入学
-                        </li>
-                        <li>
-                            2019年〜現在 徳山工業高等専門学校 情報電子工学科
-                            在学中
-                        </li>
-                    </ul>
-                </article>
-            </section>
+        <div className='pt-16 p-8'>
+            <div className='border-teal-100 border-4 rounded-xl shadow-xl p-4'>
+                <h2 className='font-medium text-gray-600 text-3xl p-4'>
+                    {`${prop.title}`}
+                </h2>
+                <hr />
+                {prop.children}
+            </div>
         </div>
     )
 }
