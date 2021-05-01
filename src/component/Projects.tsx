c334e0469578385a8ad7382ada40cdaf88314f1a
tyautyau56 <tyautyau56@gmail.com>
2021-05-01 22:23:59 +0900

    change Projects.tsx

--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -5,10 +5,18 @@ import { PROJECTS_DETAIL as pj_detail } from './store/projectdetail'
 
 const Projects: React.VFC = () => {
     return (
-        <div className='min-h-screen flex justify-center flex-wrap'>
-            {pj_detail.map((value, index) => {
-                return <Card {...value} key={index} />
-            })}
+        <div className='pt-16 p-8'>
+            <div className='border-teal-100 border-4 rounded-xl shadow-xl p-4'>
+                <h2 className='font-medium text-gray-600 text-3xl p-4'>
+                    Projects
+                </h2>
+                <hr />
+                <div className='flex justify-center flex-wrap'>
+                    {pj_detail.map((value, index) => {
+                        return <Card {...value} key={index} />
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
